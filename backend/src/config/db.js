291f3327108941df5169a9d52021cd5208d42b55
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-29 22:38:32 +0000

    connect mongoDB

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`MongoDb connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(`Error connecting to MongoDb: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
