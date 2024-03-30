edeb613406ec6fa8e5dc4ebb35965861cdf611ac
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-30 23:55:26 +0000

    implement authentcationHook

---
+++ frontend/src/hooks/useAuthenticate.jsx
@@ -0,0 +1,57 @@
+import useToken from './useToken';
+
+const useAuthenticate = () => {
+  const { saveTokens } = useToken;
+
+  const serverRegister = async (userData) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.REACT_APP_API_URI}register`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(userData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('registration failed');
+      }
+      const data = await response.json();
+      const { token, refreshToken, tokenExpiration } = data;
+      if (!token || !refreshToken) {
+        throw new Error('failed to get tokens');
+      }
+      saveTokens(token, refreshToken, tokenExpiration);
+    } catch (err) {
+      console.error('Registration error:', err);
+    }
+  };
+
+  const serverLogin = async (userData) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.REACT_APP_API_URI}login`,
+        {
+          method: 'POST',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify(userData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('registration failed');
+      }
+      const data = await response.json();
+      const { token, refreshToken, tokenExpiration } = data;
+      if (!token || !refreshToken) {
+        throw new Error('failed to get tokens');
+      }
+      saveTokens(token, refreshToken, tokenExpiration);
+    } catch (err) {
+      console.error('Login error:', err);
+    }
+  };
+
+  return { serverLogin, serverRegister };
+};
+
+export default useAuthenticate;
