054598e851371c42a904412e887778f78b39953d
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-30 23:53:41 +0000

    add element

--- backend/src/models/Client.model.js
+++ backend/src/models/Client.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const clientSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  city: { type: String },
   school: { type: String },
   age: { type: Number }
 });
