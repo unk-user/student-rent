2815ca42cdcb5b4c7330eaebcf3b49a7e1693205
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-31 23:00:11 +0000

    add citiesRoutes and error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-const privateRoutes = require('./routes/privateRoutes')
+const privateRoutes = require('./routes/privateRoutes');
+const citiesDataRoutes = require('./routes/citiesDataRoute');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -13,10 +14,26 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan());
+app.use(morgan('dev'));
 
 app.use('/api', authRoutes);
 app.use('/api/home', privateRoutes);
+app.use('/api/cities', citiesDataRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`app started on port: ${process.env.PORT}`);
