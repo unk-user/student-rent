20f1c746c18b59570022129c8dc2f9e836cf2e26
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-02 22:30:18 +0000

    move router to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SignUp from './routes/SignUp.jsx';
 import './index.css';
-import SignIn from './routes/SignIn.jsx';
-import PrivateRoute from './routes/PrivateRoute.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/Signup',
-    element: <SignUp />,
-  },
-  {
-    path: '/SignIn',
-    element: <SignIn />
-  },
-  {
-    path: '/private',
-    element: <PrivateRoute />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
