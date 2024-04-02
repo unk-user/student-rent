4f519585c21f30e2d8aca3d92f1d44f8b7b347cf
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-02 22:36:46 +0000

    init routes

---
+++ frontend/src/routes/Login.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "../components/login/LoginForm"
+
+function Login() {
+
+
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <LoginForm />
+    </main>
+  )
+}
+
+export default Login
---
+++ frontend/src/routes/Signup.jsx
@@ -0,0 +1,12 @@
+import SignupForm from "../components/signup/SignupForm"
+
+function Signup() {
+
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <SignupForm />
+    </main>
+  )
+}
+
+export default Signup
