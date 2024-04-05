69630a75b5c2eb8e1d5894b86738906446786f08
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-05 23:39:28 +0000

    add cookie-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 const privateRoutes = require('./routes/privateRoutes');
 const dotenv = require('dotenv');
@@ -13,6 +14,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 app.use('/api', authRoutes);
 app.use('/api/home', privateRoutes);
