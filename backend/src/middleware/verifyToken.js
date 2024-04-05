79253957a5e399267b2b73c31b1666b4169e713a
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-05 23:41:45 +0000

    update secret

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User.model')
 
 const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    res.status(401).json({ message: 'Unauthorized' });
+    res.sendStatus(401)
   }
-
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
-    req.userId = decoded.userId;
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const userId = decoded.userId;
+    req.userId = userId;
     req.role = decoded.role;
+    const user = await User.findOne({ _id: userId });
+    if(!user) return res.sendStatus(403);
     next();
   } catch (error) {
     console.error('invalid token');
