c2fbaccfe1e39d269986fa6ef62d8c84c715c4ab
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-08 22:50:36 +0000

    implement new refreshAccessToken logic

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -4,36 +4,53 @@ const {
   generateAccessToken,
   generateRefreshToken,
   hashPassword,
-  rotateRefreshToken,
 } = require('../helpers/authHelpers');
 const User = require('../models/User.model');
 const Client = require('../models/Client.model');
 const Landlord = require('../models/Landlord.model');
 
 const loginUser = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(401).json({ message: 'User not found' });
     }
+
     const match = await bcrypt.compare(password, user.hash);
     if (!match) {
       return res.status(403).json({ message: 'wrong password' });
     }
 
-    const token = generateAccessToken(user._id, user.role);
-    const refreshToken = generateRefreshToken(user._id, user.role);
-    user.refreshTokens.push(refreshToken);
-    await user.save();
+    const accessToken = generateAccessToken(user._id, user.role);
+    const newRefreshToken = generateRefreshToken(user._id, user.role);
+    const newRefreshTokenArray = !refreshToken
+      ? user.refreshTokens
+      : user.refreshTokens.filter((rt) => rt !== refreshToken);
+    if (refreshToken)
+      res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+      });
 
-    res.cookie('refreshToken', refreshToken, {
+    user.refreshTokens = [...newRefreshTokenArray, newRefreshToken];
+    const result = await user.save();
+    console.log(result);
+
+    res.cookie('refreshToken', newRefreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
-      token,
+      accessToken,
+      username: user.username,
+      role: user.role,
+      userId: user._id,
     });
   } catch (error) {
     console.error('login error:', error);
@@ -42,7 +59,7 @@ const loginUser = async (req, res) => {
 
 const registerUser = async (req, res) => {
   console.log(req.body);
-  const { username, email, password, role, school, age, city } = req.body;
+  const { username, email, password, role, school, city } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -76,8 +93,11 @@ const registerUser = async (req, res) => {
 
       await newClient.save();
     }
-    const token = generateAccessToken(newUser._id, newUser.role);
+
+    const accessToken = generateAccessToken(newUser._id, newUser.role);
     const refreshToken = generateRefreshToken(newUser._id, newUser.role);
+    newUser.refreshTokens = [refreshToken];
+    await newUser.save();
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
@@ -85,7 +105,10 @@ const registerUser = async (req, res) => {
       sameSite: 'strict',
     });
     res.status(201).json({
-      token,
+      accessToken,
+      username,
+      role,
+      userId: newUser._id,
     });
   } catch (error) {
     console.error(error.message);
@@ -95,44 +118,109 @@ const registerUser = async (req, res) => {
 
 const refreshAccessToken = async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) res.status(401);
-
-  const user = await User.findOne({ refreshTokens: refreshToken });
-  if (!user) return res.sendStatus(403);
-
-  try {
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-  } catch (error) {
-    res.sendStatus(403);
-  }
-  const newRefreshToken = await rotateRefreshToken(user, refreshToken);
-  if (!newRefreshToken) return res.sendStatus(403);
-
-  const accessToken = generateAccessToken(user._id, user.role);
-
-  res.cookie('refreshToken', newRefreshToken, {
+  if (!refreshToken) return res.sendStatus(401);
+  res.clearCookie('refreshToken', {
     httpOnly: true,
+    sameSite: 'None',
     secure: true,
-    sameSite: 'strict',
   });
 
-  res.json({ accessToken });
+  const user = await User.findOne({ refreshTokens: refreshToken }).exec();
+
+  //Detected reuse!
+  if (!user) {
+    console.log('cannot find user');
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) return; //Forbiden
+        const hackedUser = await User.findById(decoded.userId);
+        hackedUser.refreshTokens = [];
+        const result = await hackedUser.save();
+        console.log(result);
+      }
+    );
+    console.log('error here');
+    return res.sendStatus(403);
+  }
+
+  const username = user.username;
+  const role = user.role;
+  const userId = user._id;
+  const newRefreshTokenArray = user.refreshTokens.filter(
+    (rt) => rt !== refreshToken
+  );
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        console.log('error verify token');
+        user.refreshTokens = [...newRefreshTokenArray];
+        const result = await user.save();
+      }
+      if (err || user._id.toString() !== decoded.userId) {
+        return res.sendStatus(403);
+      }
+      const accessToken = generateAccessToken(decoded.userId, decoded.role);
+      const newRefreshToken = generateRefreshToken(
+        decoded.userId,
+        decoded.role
+      );
+
+      const userVersion = user.__v;
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id, __v: userVersion },
+        {
+          $set: { refreshTokens: [...newRefreshTokenArray, newRefreshToken] },
+          $inc: { __v: 1 },
+        },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        console.log('Conflict detected during refresh token update');
+        return res.sendStatus(409); // Conflict error
+      }
+      await updatedUser.save();
+
+      res.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+      });
+
+      return res.json({ accessToken, username, role, userId });
+    }
+  );
 };
 
 const logoutUser = async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) return res.sendStatus(204);
+  if (!refreshToken) return res.sendStatus(204); //No content
 
-  const user = await User.findOne({ refreshTokens: refreshToken });
-  if (!user) return res.sendStatus(204);
+  const user = await User.findOne({ refreshToken }).exec();
+  if (!user) {
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+    });
+    return res.sendStatus(204);
+  }
 
-  user.refreshTokens = user.refreshTokens.filter(
-    (token) => token !== refreshToken
-  );
-  await user.save();
-
-  res.clearCookie('refreshToken');
-  res.sendStatus(204);
+  user.refreshTokens = user.refreshTokens.filter((rt) => rt !== refreshToken);
+  const result = await user.save();
+  console.log(result);
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true,
+  });
+  return res.sendStatus(204);
 };
 
 module.exports = {
