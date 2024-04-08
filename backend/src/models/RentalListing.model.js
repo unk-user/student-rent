7445756e0512590aeeb2cb2b19a20793150123b5
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-08 22:52:58 +0000

    add post mongoose middleware

--- backend/src/models/RentalListing.model.js
+++ backend/src/models/RentalListing.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Landlord = require('../models/Landlord.model');
 const Schema = mongoose.Schema;
 
 const rentalListingSchema = new Schema({
@@ -9,6 +10,17 @@ const rentalListingSchema = new Schema({
   rooms: { type: Number, required: true },
   images: [{type: String}],
   landlordId: { type: Schema.Types.ObjectId, ref: 'Landlord', required: true }
+});
+
+rentalListingSchema.post('save', async (rentalListing, next) => {
+  try {
+    const landlord = await Landlord.findById(rentalListing.landlordId);
+    landlord.properties.push(rentalListing._id);
+    await landlord.save();
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model('RentalListing', rentalListingSchema);
