f45d79e4e0d12d443c0b44823b11dc7d3bc99644
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-08 22:52:18 +0000

    fix next callback

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -1,23 +1,25 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User.model')
+const User = require('../models/User.model');
 
 const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    res.sendStatus(401)
+    res.sendStatus(401);
   }
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const userId = decoded.userId;
+    const role = decoded.role;
     req.userId = userId;
-    req.role = decoded.role;
+    req.role = role;
     const user = await User.findOne({ _id: userId });
-    if(!user) return res.sendStatus(403);
-    next();
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
   } catch (error) {
-    console.error('invalid token');
-    return res.status(403).json({message: 'Invalid token'});
+    return res.status(403).json({ message: 'Invalid token' });
   }
+  next();
 };
 
 module.exports = verifyToken;
