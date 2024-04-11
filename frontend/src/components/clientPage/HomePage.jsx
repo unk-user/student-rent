f8ad709a5de521784822231c79dcc646cd180be9
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-11 23:46:06 +0000

    init homepage

---
+++ frontend/src/components/clientPage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Searchbar from './Searchbar';
+
+function HomePage() {
+  return (
+    <>
+      <header className="h-[68px] bg-slate-800">
+        <Searchbar />
+      </header>
+      <main>Hey</main>
+    </>
+  );
+}
+
+export default HomePage;
