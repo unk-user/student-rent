ecd95239b0080d6a976f64dd6b98b314965280b0
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-12 23:59:41 +0000

    update routes

--- backend/src/routes/landlordRoutes.js
+++ backend/src/routes/landlordRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const verifyToken = require('../middleware/verifyToken');
-const { getListings, createListing } = require('../controllers/landlordControllers');
+const {
+  getListings,
+  createListing,
+  getListing,
+  updateListing,
+  addStudent,
+  deleteStudent,
+  deleteListing,
+} = require('../controllers/landlordControllers');
 const router = express.Router();
 
 router.use(verifyToken);
 router.get('/listings', getListings);
 router.post('/listings', createListing);
+
+router.get('/listings/:listingId', getListing);
+router.put('/listings/:listingId', updateListing);
+router.delete('listings/:listingId', deleteListing);
+
+router.post('/listings/:listingId/students', addStudent);
+router.delete('/listings/:listingId/students', deleteStudent);
 
 module.exports = router;
