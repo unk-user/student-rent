733f07721d80a1e236a20ea26c5ce3506e99c650
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-12 23:58:35 +0000

    implement dashboard endpoints

--- backend/src/controllers/landlordControllers.js
+++ backend/src/controllers/landlordControllers.js
@@ -1,14 +1,20 @@
 const Landlord = require('../models/Landlord.model');
 const User = require('../models/User.model');
 const RentalListing = require('../models/RentalListing.model');
+const Client = require('../models/Client.model');
 const Booking = require('../models/Booking.model');
 
 const getListings = async (req, res) => {
   const userId = req.userId;
   try {
-    const landlord = await Landlord.findOne({ userId: userId }).populate(
-      'properties'
-    );
+    const landlord = await Landlord.findOne({ userId }).populate({
+      path: 'properties',
+      model: 'RentalListing',
+      populate: {
+        path: 'students',
+        model: 'Client',
+      },
+    });
     if (!landlord) return res.sendStatus(403);
     const listings = landlord.properties;
     return res.json({ listings });
@@ -18,10 +24,29 @@ const getListings = async (req, res) => {
   }
 };
 
+const getListing = async (req, res) => {
+  const userId = req.userId;
+  const { listingId } = req.params;
+  try {
+    const rentalListing = await RentalListing.findById(listingId).populate(
+      'students'
+    );
+    const landlord = await Landlord.findOne({ userId });
+    const landlordId = landlord._id;
+    if (!rentalListing || !landlord)
+      return res.status(404).json({ userId, listingId });
+    if (!rentalListing.landlordId.equals(landlordId)) return res.sendStatus(403);
+    return res.json({ rentalListing });
+  } catch (error) {
+    console.error('error fetching listing:', error);
+    return res.json({ error });
+  }
+};
+
 const createListing = async (req, res) => {
   const userId = req.userId;
   try {
-    const landlord = await Landlord.findOne({ userId: userId });
+    const landlord = await Landlord.findOne({ userId });
     const landlordId = landlord._id;
     const newListing = new RentalListing({ ...req.body, landlordId });
     await newListing.save();
@@ -35,4 +60,89 @@ const createListing = async (req, res) => {
   }
 };
 
-module.exports = { getListings, createListing };
+const updateListing = async (req, res) => {
+  const { listingId } = req.params;
+  const { title, description, location, price, rooms, students, images } =
+    req.body;
+
+  try {
+    const rentalListing = await RentalListing.findByIdAndUpdate(
+      listingId,
+      {
+        title,
+        description,
+        location,
+        price,
+        rooms,
+        students,
+        images,
+      },
+      { new: true, runValidators: true }
+    );
+    if (!rentalListing) {
+      return res.status(404).json({ error: 'Rental listing not found' });
+    }
+
+    res.json(rentalListing);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+const addStudent = async (req, res) => {
+  const { clientId, listingId } = req.body;
+  try {
+    const student = await Client.findById(clientId);
+    const listing = await RentalListing.findById(listingId);
+    if (!student || !listing) return res.sendStatus(404);
+    if (listing?.students.includes(clientId)) return res.sendStatus(400);
+    listing.students.push(clientId);
+    await listing.save();
+    return res.json(listing);
+  } catch (error) {
+    console.error(`error adding student ${clientId}:`, error);
+    return res.sendStatus(500);
+  }
+};
+
+const deleteListing = async (req, res) => {
+  const { listingId } = req.params;
+  try {
+    const deletedListing = RentalListing.findByIdAndDelete(listingId);
+    if (!deletedListing) return res.sendStatus(404);
+    return res
+      .status(204)
+      .json({ message: 'successfully deleted', deleteListing });
+  } catch (error) {
+    console.error(`error deleting rental listing:`, error);
+    return res.json('error deleting property');
+  }
+};
+
+const deleteStudent = async (req, res) => {
+  const { listingId } = req.params;
+  const { clientId } = req.body;
+  try {
+    const listing = RentalListing.findById(listingId);
+    if (!listing) return res.sendStatus(404);
+    const updatedStudentList = listing.students.filter(
+      (student) => student !== clientId
+    );
+    listing.students = updatedStudentList;
+    await listing.save();
+    return res.status(204).json({ clientId });
+  } catch (error) {
+    console.error(`error removing student:`, error);
+    return res.json('error removing student from rental listing');
+  }
+};
+
+module.exports = {
+  getListings,
+  getListing,
+  createListing,
+  updateListing,
+  addStudent,
+  deleteStudent,
+  deleteListing,
+};
