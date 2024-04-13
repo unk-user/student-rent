1f82f60428509a6dcecbcb7a9a122e6d98bc3d02
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-13 23:42:17 +0000

    add renting period

--- backend/src/models/RentalListing.model.js
+++ backend/src/models/RentalListing.model.js
@@ -6,11 +6,13 @@ const rentalListingSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   location: { type: String, required: true },
+  period: { type: String, enum: ['monthly', 'yearly'], default: 'monthly' },
   price: { type: Number, required: true },
   rooms: { type: Number, required: true },
-  students: [{type: Schema.Types.ObjectId, ref: 'Client'}],
-  images: [{type: String}],
-  landlordId: { type: Schema.Types.ObjectId, ref: 'Landlord', required: true }
+  bathrooms: { type: Number },
+  students: [{ type: Schema.Types.ObjectId, ref: 'Client' }],
+  images: [{ type: String }],
+  landlordId: { type: Schema.Types.ObjectId, ref: 'Landlord', required: true },
 });
 
 rentalListingSchema.post('save', async (rentalListing, next) => {
