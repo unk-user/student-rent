caa0b4087197c37dae606cbb5fad733dcc988e68
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-13 23:41:47 +0000

    remove date requirement

--- backend/src/models/Booking.model.js
+++ backend/src/models/Booking.model.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const bookingSchema = new Schema({
   clientId: { type: Schema.Types.ObjectId, ref: 'Client', required: true },
-  rentalListingId: { type: Schema.Types.ObjectId, ref: 'RentalListing', required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true }
-})
+  rentalListingId: {
+    type: Schema.Types.ObjectId,
+    ref: 'RentalListing',
+    required: true,
+  },
+  startDate: Date,
+  endDate: Date,
+});
 
 module.exports = mongoose.model('Booking', bookingSchema);
