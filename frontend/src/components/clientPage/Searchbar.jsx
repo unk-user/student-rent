9840db64116e138daab014b6f51ee73ea9f932d7
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-18 19:58:57 +0100

    add category filtering

--- frontend/src/components/clientPage/Searchbar.jsx
+++ frontend/src/components/clientPage/Searchbar.jsx
@@ -1,21 +1,27 @@
 import ComboBox from '../ui/ComboBox';
 import { IoIosSearch } from 'react-icons/io';
 
-function Searchbar() {
+function Searchbar({ setCategory }) {
+
+  const handleChange = (value) => {
+    setCategory(value);
+  }
+
   return (
     <div className="flex items-center h-11 rounded-md py-2 px-2 bg-white max-w-[480px]">
       <IoIosSearch className="text-[1.7rem]" />
       <input
         type="text"
-        className="h-10 py-2 px-2 flex-1 text-[1.2rem] outline-none"
+        className="h-10 py-2 px-2 flex-1 text-[1.2rem] outline-none max-sm:w-48"
         placeholder="Search here"
-      /><div className='h-9'>
-        
+      />
+      <div className="h-9">
         <ComboBox
-          options={['All', 'Room', 'Appartement', 'Studio']}
-          initialValue='All'
-          className={'bg-[#F6CA45] text-[1.1rem]'}
+          options={['All', 'Room', 'Appartment', 'Studio']}
+          initialValue="All"
+          className={'bg-[#F6CA45] text-[1.1rem] max-sm:w-24'}
           withSearch={false}
+          handleChange={handleChange}
         />
       </div>
     </div>
