9192c1687ed8c0980689ea91a75c93a66cb9f046
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-23 23:03:54 +0100

    add react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import { AuthProvider } from './context/AuthProvider.jsx';
 import App from './App.jsx';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
