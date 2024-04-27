f1d31608a4ca0a6c0f22fb3b5a1b1a70b7bfeb95
Hamza <144457566+unk-user@users.noreply.github.com>
2024-04-27 20:22:49 +0100

    implement react query

--- frontend/src/components/ListingPage/ListingPage.jsx
+++ frontend/src/components/ListingPage/ListingPage.jsx
@@ -1,84 +1,44 @@
-import axios from 'axios';
-import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import AuthContext from '../../context/AuthProvider';
+import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 import BookingCard from './BookingCard';
 import PreviewImages from '../ui/PreviewImages';
 import Facilities from './Facilities';
 import LandlordCard from '../ui/LandlordCard';
 import Reviews from './Reviews';
+import useQueryAuth from '../../hooks/useQueryAuth';
 
 function ListingPage() {
   const { listingId } = useParams();
-  const { auth, refreshAccessToken } = useContext(AuthContext);
-  const [listing, setListing] = useState(null);
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-  const landlordProperties = useMemo(
-    () => listing && listing.landlordId.properties.length
-  , [listing]);
 
-  const fetchListing = useCallback(async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.REACT_APP_API_URI}student/listings/${listingId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${auth.accessToken}`,
-          },
-        }
-      );
-      console.log(response);
-      setListing(response.data.listing);
-    } catch (error) {
-      console.error('error fetching listingData:', error);
-    }
-  }, [auth, listingId]);
+  const { data, status } = useQueryAuth({
+    queryKey: [listingId],
+    url: `student/listings/${listingId}`
+  });
 
-  useEffect(() => {
-    const refreshLocalAuth = async () => {
-      if (!auth) {
-        try {
-          const newAuth = await refreshAccessToken();
-          console.log(newAuth);
-          if (!newAuth) {
-            setShouldRedirect(true);
-          }
-        } catch (error) {
-          setShouldRedirect(true);
-          console.error(error);
-        }
-      }
-    };
-
-    refreshLocalAuth();
-    fetchListing();
-
-    return () => {
-      setShouldRedirect(false);
-    };
-  }, [fetchListing, auth, refreshAccessToken]);
-
-  if (shouldRedirect) return <Navigate to="/login" replace />;
+  const landlordPropertiesNumber = useMemo(
+    () => data?.listing && data.listing.landlordId.properties.length,
+    [data]
+  );
 
   return (
     <>
-      {listing && (
+      {status === 'success' && (
         <>
           <main className="px-12">
             <header>
-              <h2>{listing.title}</h2>
-              <h5>{listing.address}</h5>
+              <h2>{data.listing.title}</h2>
+              <h5>{data.listing.address}</h5>
             </header>
             <section className="flex w-full gap-2">
               <div className="flex-1">
                 <PreviewImages />
-                <Facilities listing={listing} />
+                <Facilities listing={data.listing} />
                 <LandlordCard
-                  name={listing.landlordId.userId.username}
-                  properties={landlordProperties}
+                  name={data.listing.landlordId.userId.username}
+                  properties={landlordPropertiesNumber}
                 />
               </div>
-              <BookingCard listing={listing} />
+              <BookingCard listing={data.listing} />
             </section>
             <div>
               <Reviews />
---
+++ frontend/src/hooks/useQueryAuth.jsx
@@ -0,0 +1,59 @@
+import { useNavigate } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import axiosInstance from '../utils/axiosInstance';
+import AuthContext from '../context/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+
+function useQueryAuth({ queryKey, url }) {
+  const [authErrorCount, setAuthErrorCount] = useState(0);
+  const { auth, refreshAccessToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const fetchListing = async () => {
+    if (!auth) {
+      try {
+        const newAuth = await refreshAccessToken();
+        const response = await axiosInstance(url, {
+          headers: { Authorization: `Bearer ${newAuth.accessToken}` },
+        });
+        return response.data;
+      } catch (refreshError) {
+        navigate('/login');
+      }
+    }
+    const response = await axiosInstance(url, {
+      headers: {
+        Authorization: `Bearer ${auth.accessToken}`,
+      },
+    });
+    return response.data;
+  };
+
+  const { data, status, error } = useQuery({
+    queryFn: fetchListing,
+    queryKey: [...queryKey, auth?.accessToken],
+    enabled: true,
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (
+      status === 'error' &&
+      (error?.response.status === 401 || error?.response.status === 403) &&
+      authErrorCount < 2
+    ) {
+      console.log(authErrorCount);
+      setAuthErrorCount((prevCount) => prevCount + 1);
+      refreshAccessToken();
+    } else if (status === 'error' && authErrorCount > 1) {
+      navigate('/login', { replace: true });
+    } else if (status === 'success') {
+      setAuthErrorCount(0);
+    }
+  }, [status, navigate, error, refreshAccessToken, authErrorCount]);
+
+  return { data, status };
+}
+
+export default useQueryAuth;
