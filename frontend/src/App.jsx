824829f5fac9cdc0c772d776971353cdec035f64
Hamza <144457566+unk-user@users.noreply.github.com>
2024-06-28 22:17:07 +0100

    integrate auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,37 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from 'react-router-dom';
 import axios from 'axios';
 import AuthLayout from './pages/auth/AuthLayout';
 import LoginForm from './pages/auth/components/LoginForm';
 import RegisterForm from './pages/auth/components/RegisterForm';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthProvider';
 
 function App() {
   axios.defaults.withCredentials = true;
+  const { auth } = useContext(AuthContext);
 
   const router = createBrowserRouter([
     {
       path: '/auth',
       element: <AuthLayout />,
       children: [
-        { path: 'signin', element: <LoginForm /> },
+        {
+          path: 'signin',
+          element: auth?.accessToken ? (
+            <Navigate to="/tenant/rent" replace={true} />
+          ) : (
+            <LoginForm />
+          ),
+        },
         { path: 'signup', element: <RegisterForm /> },
       ],
+    },
+    {
+      path: 'tenant',
     },
   ]);
 
---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,81 @@
+import { createContext, useEffect, useLayoutEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import axiosInstance from '@/utils/axiosInstance';
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const initialRefresh = async () => {
+      try {
+        const response = await axiosInstance.post(`/refresh`);
+        const newAuth = response.data;
+        setAuth(newAuth);
+      } catch {
+        setAuth(null);
+      }
+    };
+
+    initialRefresh();
+  }, []);
+
+  useLayoutEffect(() => {
+    const authInterceptor = axiosInstance.interceptors.request.use((config) => {
+      config.headers.Authorization =
+        !config._retry && auth.accessToken
+          ? `Bearer ${auth.accessToken}`
+          : config.headers.Authorization;
+      return config;
+    });
+
+    return () => {
+      axiosInstance.interceptors.response.eject(authInterceptor);
+    };
+  }, [auth?.accessToken]);
+
+  useLayoutEffect(() => {
+    const refreshInterceptor = axiosInstance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (
+          error.response.status === 403 &&
+          error.response.data.message === 'Unauthorized'
+        ) {
+          try {
+            const response = await axiosInstance.post('/refresh');
+
+            setAuth(response.data);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            originalRequest._retry = true;
+
+            return axiosInstance(originalRequest);
+          } catch {
+            setAuth(null);
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      axiosInstance.interceptors.request.eject(refreshInterceptor);
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+AuthProvider.propTypes = {
+  children: propTypes.any,
+};
+
+export default AuthProvider;
